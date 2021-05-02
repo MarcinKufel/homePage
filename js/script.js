793e1775a3d02bf61027f3c9343f2677e2c92bd3
Marcin Kufel <marcinkufel84@gmail.com>
2021-05-02 22:42:37 +0200

    moved variables to functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,34 @@
-
-const welcome = () => {
-console.log("Prototype of my resume")
-}
-
-const onChangeBackgroundClick = () => {
-    bodyBackground.classList.toggle("body--bright");
-
-    if (changeBackground.innerText === "Bright mode") 
-    {
-        changeBackground.innerText = "Dark mode";
-    } 
-    else  
-    {
-        changeBackground.innerText = "Bright mode";
+{
+    const welcome = () => {
+        console.log("Prototype of my resume")
     }
-}
 
-const hideImageFunction = () => {
-    myPhoto.classList.toggle("js-navigation__image");
-
-    if (hideImage.innerText === "Hide image") 
-    {
-        hideImage.innerText = "Show image";
-    } 
-    else  
-    {
-        hideImage.innerText = "Hide image";
+    const onChangeBackgroundClick = () => {
+        const bodyBackground = document.querySelector(".js-body");
+        const changeBackground = document.querySelector(".js-changeBackground");
+        bodyBackground.classList.toggle("body--bright");
+        if (changeBackground.innerText === "Bright mode") {
+            changeBackground.innerText = "Dark mode";
+        }
+        else {
+            changeBackground.innerText = "Bright mode";
+        }
     }
+
+    const hideImageFunction = () => {
+        const myPhoto = document.querySelector(".navigation__image");
+        const hideImage = document.querySelector(".js-hideImage");
+        myPhoto.classList.toggle("js-navigation__image");
+        if (hideImage.innerText === "Hide image") {
+            hideImage.innerText = "Show image";
+        }
+        else {
+            hideImage.innerText = "Hide image";
+        }
+    }
+
+    welcome();
+    changeBackground.addEventListener("click", onChangeBackgroundClick);
+    hideImage.addEventListener("click", hideImageFunction);
+
 }
-
-
-welcome();
-
-const bodyBackground = document.querySelector(".js-body");
-const changeBackground = document.querySelector(".js-changeBackground");
-
-changeBackground.addEventListener("click", onChangeBackgroundClick);
-
-
-const myPhoto = document.querySelector(".navigation__image");
-const hideImage = document.querySelector(".js-hideImage");
-
-hideImage.addEventListener("click", hideImageFunction);
